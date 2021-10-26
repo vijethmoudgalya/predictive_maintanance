b4c2312cfb55d3b5fee15142513e013408a3d90f
Vijeth Moudgalya <vijethmoudgalya@gmail.com>
2021-10-26 12:10:03 +0530

    first_commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project objective is to enhance the maintenance operations and planning of time-based preventive maintenance by applying data science techniques and machine learning algorithms for predicting more accurate maintenance requirements.and is implemented using mlops',
+    author='vijeth_moudgalya',
+    license='MIT',
+)
